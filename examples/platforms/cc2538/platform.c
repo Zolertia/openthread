29863e1fb9e46ab6a2e8fbd70908d193997e0acb
Łukasz Duda <lukasz.duda@nordicsemi.no>
2017-02-22 18:18:24 +0100

    Move otPlatUartEnable to CLI or NCP init functions. (#1359)

--- examples/platforms/cc2538/platform.c
+++ examples/platforms/cc2538/platform.c
@@ -32,7 +32,6 @@
  *   This file includes the platform-specific initializers.
  */
 
-#include <platform/uart.h>
 #include "platform-cc2538.h"
 
 otInstance *sInstance;
@@ -59,7 +58,6 @@ void PlatformInit(int argc, char *argv[])
     cc2538AlarmInit();
     cc2538RandomInit();
     cc2538RadioInit();
-    otPlatUartEnable();
 
     PlatformLedsInit();
 
